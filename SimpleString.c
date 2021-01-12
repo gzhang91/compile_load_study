59c39c6abf0db24f12b2621fcc3eb8482e48edbe
gzhang <gzhang@126.com>
2021-01-12 18:08:05 +0800

    1. add

--- SimpleString.c
+++ SimpleString.c
@@ -1,7 +1,7 @@
 int printf(const char *format, ...);
 
 int main() {
-	char str[] = "hello world";
+	char str[] = "hello world123";
 	printf("AAAA = %s\n", str);
 	printf("my name is %s\n", "gzhang");
 	return 1;
